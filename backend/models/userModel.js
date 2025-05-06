4f20c7760c641e8e19393b1594ab5e55898f47c4
mmjavier <mmjavier5@up.edu.ph>
2025-05-07 00:59:53 +0800

    setup: Initial set-up for project directory with MongoDB, axios, tailwind, and postcss setup

---
+++ backend/models/userModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    FirstName: {
+        type: String,
+        required: true,
+    },
+    MiddleName: {
+        type: String,
+        required: false,
+    },
+    LastName: {
+        type: String,
+        required: true,
+    },
+    isMerchant: {
+        type: Boolean,
+        default: false,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    Cart: {
+        type: Array,
+        default: [],
+    },
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './styles.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className="text-green-600 font-bold text-2xl">Hello World</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
