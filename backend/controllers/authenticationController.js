af2fe41a0b9336c1cc96a8548d69ea65ebd5014c
mmjavier <mmjavier5@up.edu.ph>
2025-05-29 14:55:20 +0800

    feat: Initial implementation for cookie-based auth

--- backend/controllers/authenticationController.js
+++ backend/controllers/authenticationController.js
@@ -1,8 +1,21 @@
 
 import User from "../models/userModel.js"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
 
 //Authentication Methods
+
+//Create Token
+const createToken = (user) => {
+    return jwt.sign(
+        {
+            email: user.email,
+            userType: user.isMerchant ? "Merchant" : "Customer"
+        },
+        process.env.JWT_SECRET,
+        {expiresIn: process.env.TOKEN_EXPIRATION}
+    )
+}
 
 //Create Account (Sign Up)
 const signUp = async (req, res) => {
@@ -38,4 +51,45 @@ const signUp = async (req, res) => {
     }
 };
 
-export {signUp};
+//Function for Logging In
+const login = async (req, res) => {
+
+    try {
+        const {email, password} = req.body;
+        console.log(`email: ${email}, password: ${password}`);
+        if(!email || !password){
+            return res.status(401).json({message: 'All fields are Required'});
+        }
+        //Look for user in database
+        const user = await User.findOne({email});
+
+        //Validate credentials
+        if(!user || !(await bcrypt.compare(password, user.password))){
+            console.log("User not Found")
+            return res.status(401).json({message: 'Invalid Credentials'});
+        }
+
+        //Generate Token
+        const token = createToken(user);
+        console.log(token);
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            secure: process.env.COOKIE_SECURE,
+            maxAge: process.env.COOKIE_MAX_AGE,
+            sameSite: 'Lax'
+        });
+
+        res.status(201).json({message: "Logged In Successfully", token})
+
+    } catch (error) {
+        res.status(500).json({message: "Server Error", error})
+    }
+}
+
+const logout = (req,res) => {
+    res.clearCookie('token');
+    res.status(200).json({message:"Logged out Successfully"})
+}
+
+export {signUp, login, logout};
--- backend/routers/authenticationRouter.js
+++ backend/routers/authenticationRouter.js
@@ -1,7 +1,9 @@
-import {signUp} from "../controllers/authenticationController.js"
+import {signUp, login, logout} from "../controllers/authenticationController.js"
 
 const authenticationRouter = (app) => {
     app.post("/api/auth/signup", signUp);
+    app.post("/api/auth/login", login);
+    app.post("/api/auth/logout", logout);
 }
 
 export default authenticationRouter;
