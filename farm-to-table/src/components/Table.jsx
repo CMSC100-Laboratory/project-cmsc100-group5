860447ba2bea707fd0f118fbe2d0e85e0abb5d1a
jdde19 <estadillajohndavid@gmail.com>
2025-05-29 22:49:56 +0800

    added qunatity column

--- farm-to-table/src/components/Table.jsx
+++ farm-to-table/src/components/Table.jsx
@@ -1,7 +1,8 @@
-function Table(name, sales) {
+function Table(name, sales, quantity) {
   return (
     <tr>
       <td className="px-6 py-4 text-sm text-gray-800 text-center">{name}</td>
+      <td className="px-6 py-4 text-sm text-gray-800 text-center">{quantity}</td>
       <td className="px-6 py-4 text-sm text-gray-800 text-center">₱{sales}</td>
     </tr>
 
